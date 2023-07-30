e5b602b17cab0519009914f49394c9f0d4877f0f
Haltoen <hjaltipp@hotmail.com>
2023-07-30 15:02:57 +0200

    can add skills again

--- src/UI.py
+++ src/UI.py
@@ -1,83 +1,104 @@
 import tkinter as tk
+import database
 
-class Page(tk.Frame):
+db = database.db
+class Page(tk.Frame): 
     def __init__(self, parent, text):
         super().__init__(parent)
         self.parent = parent
-
-        label = tk.Label(self, text=text)
-        label.pack()
-
-class Page(tk.Frame):
-    def __init__(self, parent, text):
-        super().__init__(parent)
-        self.parent = parent
-
         self.label = tk.Label(self, text=text)
         self.label.pack()
+        self.db = db
+        
+    def create_submit_field(self, cmd, name):
+        input_frame = tk.Frame(self, background="grey", borderwidth=10)
+        input_frame.pack()
+
+        if name:
+            label = tk.Label(input_frame, text=name)
+            label.pack(side=tk.TOP)
+
+        self.text_field = tk.Entry(input_frame)
+        self.text_field.pack(pady=10, side=tk.LEFT)
+        self.submit_button = tk.Button(
+            input_frame,
+            text="Submit",
+            command = lambda : submitfield(self, cmd)
+        )
+        self.submit_button.pack(side=tk.RIGHT)
+
+        def submitfield(self, command):
+            user_input = self.text_field.get()
+            print(user_input)
+            lambda user_input : command(self, user_input)  # Pass the user input as the argument to the command function
+            self.text_field.delete(0, tk.END)
+
+    
+    def create_list_field(self, lst: list) -> None:
+        list_field = tk.Frame(self, background = "white", borderwidth=3 )
+
+
+## pages
 
 class HomePage(Page):
     def __init__(self, parent):
         super().__init__(parent, "Welcome to the Home Page")
+        self.input_field = tk.Entry(self)  # Create the input field
+        self.input_field.pack()
+        self.button_get_input = tk.Button(self, text="Get Input", command=self.get_input_text)
+        self.button_get_input.pack()
 
-        self.button_change_text = tk.Button(self, text="Change Text", command=self.change_text)
-        self.button_change_text.pack()
+    def get_input_text(self):
+        user_input = self.input_field.get()
+        print("User input:", user_input)
 
-    def change_text(self):
-        new_text = "New text on the Home Page"
-        self.label.config(text=new_text)
-
-class AboutPage(Page):
+class Resume (Page):
     def __init__(self, parent):
-        super().__init__(parent, "This is the About Page")
+        super().__init__(parent, "This is the Resume Page")
 
-        self.button_change_text = tk.Button(self, text="Change Text", command=self.change_text)
-        self.button_change_text.pack()
+        #self.create_submit_field(db.add_skill, "add Resume")
 
-    def change_text(self):
-        new_text = "New text on the About Page"
-        self.label.config(text=new_text)
+class Skills (Page):
+    def __init__(self, parent):
+        super().__init__(parent, "This is the skills Page")
+        #self.skills=db.skill_list()
+
+        self.create_submit_field(db.add_skill, name="add skills")
+
+class CoverLetter (Page):
+    def __init__(self, parent):
+        super().__init__(parent, "This is the coverletter Page")
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("My App")
         self.geometry("800x600")
-
-        self.pages = [HomePage, AboutPage]
-        self.current_page_index = 0
-
+        self.pages = [HomePage, Resume, Skills, CoverLetter]
+        self.current_page=HomePage
         self.create_navigation_buttons()
         self.show_current_page()
+        self.db = db
 
     def create_navigation_buttons(self):
-        prev_button = tk.Button(self, text="Previous", command=self.show_previous_page)
-        prev_button.pack(side=tk.LEFT)
+        button_frame = tk.Frame(self)
+        button_frame.pack(side=tk.BOTTOM)
+        for  page in self.pages:
+            name = page.__name__
+            print(name)
+            new_button = tk.Button(button_frame, text=name, command=lambda p=page: self.change_page(p))
+            new_button.pack(side=tk.LEFT)
 
-        next_button = tk.Button(self, text="Next", command=self.show_next_page)
-        next_button.pack(side=tk.RIGHT)
-
-    def show_previous_page(self):
-        self.current_page_index -= 1
-        if self.current_page_index < 0:
-            self.current_page_index = len(self.pages) - 1
-        self.show_current_page()
-
-    def show_next_page(self):
-        self.current_page_index += 1
-        if self.current_page_index >= len(self.pages):
-            self.current_page_index = 0
+    def change_page(self, new_page):
+        print(self.current_page)
+        self.current_page.pack_forget()  # Remove the old page from the window
+        self.current_page = new_page
         self.show_current_page()
 
     def show_current_page(self):
-        new_page_class = self.pages[self.current_page_index]
-
-        if hasattr(self, "current_page"):
-            self.current_page.pack_forget()  # Remove the old page from the window
-
+        new_page_class = self.current_page
         self.current_page = new_page_class(self)
         self.current_page.pack(fill=tk.BOTH, expand=True)
-
 
 if __name__ == "__main__":
     app = App()
--- src/database.py
+++ src/database.py
@@ -5,6 +5,7 @@ import os
 class db:
     def __init__(self):
         self.setup_complete=False
+        self.setup_tables
         
     def execute_query (self, inp_conn, inp_cursor, query: str, params=None)-> None:
         if params:
@@ -14,13 +15,14 @@ class db:
         inp_conn.commit()
         return None
     
-    def single_query (self, query: str, params) -> None:
+    def single_query (self, query: str, params = None) -> None:
         conn = sql3.connect("database.db") #connect/create db file
         cursor = conn.cursor() # used to execute SQL queries
         self.execute_query(conn, cursor, query, params)
         conn.commit()
         conn.close()
         return None
+    
     """
     def multi_query (self, query_lst: list) -> None: 
         conn = sql3.connect("database.db") #connect/create db file
@@ -44,59 +46,28 @@ class db:
                 skill TEXT PRIMARY KEY
             )
         """
-        self.multi_query([query1, query2]) 
-        return None
+        self.single_query(query1)
+        self.single_query(query2)
+        return None 
 
     def add_resume(self, content: str, language: str) -> None:
         query = "INSERT INTO Resume (content, language) VALUES (?, ?)"
         self.single_query(query, (content, language))
         return None
 
-    def add_skills(self, skill: str) -> None:
+    def add_skill(self, skill: str) -> None:
         query = "INSERT INTO Skills (skill) VALUES (?)"
         self.single_query(query, (skill,))  
         return None   
     
+    def skill_list(self) -> list[str]:
+        query = "SELECT skill FROM Skills"
+        res = self.single_query(self, query)
+        print (res)
+
     def delete_database(self):
         if os.path.exists("database.db"):
             os.remove("database.db")
         return None
     
 
-
-   
-
-    
-def simple_test():
-    # Create a new instance of the db class
-    my_db = db()
-
-    # Setup the tables
-    my_db.delete_database()
-    my_db.setup_tables()
-
-    # Add some resumes and skills
-    my_db.add_resume("Resume 1 content", "English")
-    my_db.add_resume("Resume 2 content", "French")
-
-    my_db.add_skills("Python")
-    my_db.add_skills("JavaScript")
-
-    # Retrieve and print the data
-    with sql3.connect("database.db") as conn:
-        cursor = conn.cursor()
-
-        # Retrieve data from the Resume table
-        cursor.execute("SELECT * FROM Resume")
-        resume_data = cursor.fetchall()
-        print("Resume Data:")
-        print(resume_data)
-
-        # Retrieve data from the Skills table
-        cursor.execute("SELECT * FROM Skills")
-        skills_data = cursor.fetchall()
-        print("\nSkills Data:")
-        print(skills_data)
-
-simple_test()
-    
